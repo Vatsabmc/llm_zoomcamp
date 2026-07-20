a5ae60fbcf9a4304ee3bafdceff312422e0ecc2f
vatsabmc <vatsabmc@gmail.com>
2026-07-20 14:52:30 +0200

    Add files monitoring lesson

---
+++ 05-monitoring/assistant.py
@@ -0,0 +1,29 @@
+import sys
+
+from db_save import save_conversation
+from dotenv import load_dotenv
+from ingest import build_index, load_faq_data
+from metrics import RAGWithMetrics
+from openai import OpenAI
+
+
+def create_assistant():
+    load_dotenv()
+
+    documents = load_faq_data()
+    index = build_index(documents)
+
+    return RAGWithMetrics(index=index, llm_client=OpenAI())
+
+
+if __name__ == "__main__":
+    assistant = create_assistant()
+
+    query = "How do I join the course?"
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
+
+    answer = assistant.rag(query)
+    print(answer)
+
+    save_conversation(assistant.last_call, query, "llm-zoomcamp")
